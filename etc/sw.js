4eaa8d455137526b4293a680551941eb05e01833
Roman Doroshenko <roman.doroshenko.tlt@gmail.com>
2018-01-19 09:14:24 +0000

    hey

--- etc/sw.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-importScripts('sw-toolbox.js');
-toolbox.precache(["index.html","styles.css","bundle.js"]);
-toolbox.router.get('/images/*', toolbox.cacheFirst);
-toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
